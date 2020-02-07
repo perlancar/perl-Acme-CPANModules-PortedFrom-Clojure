7751def10923b85e347aa013e0a4bc51fb43a6e5
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-07 07:02:49 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/PortedFrom/Clojure.pm
@@ -0,0 +1,27 @@
+package Acme::CPANModules::PortedFrom::Clojure;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => "Modules/applications that are ported from (or inspired by) ".
+        "Clojure",
+    description => <<'_',
+
+If you know of others, please drop me a message.
+
+_
+    entries => [
+        {
+            module => 'String::CamelSnakeKebab',
+            summary => 'camel-snake-kebab',
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
